aa57ed3dca12c0421705c9504260715ae3592d6e
user <user@DESKTOP-IBC1EFO>
2025-01-01 17:24:30 +0200

    .

--- src/main/java/FeaturesMain/UserClassExtraCoverage.java
+++ src/main/java/FeaturesMain/UserClassExtraCoverage.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import io.cucumber.java.AfterAll;
-import io.cucumber.java.BeforeAll;
 
 class UserClassExtraCoverage {
 
